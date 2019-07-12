3909a3da0a3025beee42f09ac362503cdc325a29
Anthony <vuonga91@gmail.com>
2019-07-12 11:38:47 -0700

    VPC modularized

---
+++ modules/vpc/main.tf
@@ -0,0 +1,42 @@
+# Populate state file with AZ info.
+
+data "aws_availability_zones" "available" {}
+
+#############################################################################################################
+# Create VPC network
+#############################################################################################################
+resource "aws_vpc" "main" {
+  cidr_block = "${var.custom_cidr_block}"
+}
+
+#############################################################################################################
+# Create public subnet to attach Internet Gateway route
+#############################################################################################################
+resource "aws_subnet" "public" {
+  count                   = "${var.subnet_count}"
+  vpc_id                  = "${aws_vpc.main.id}"
+  cidr_block              = "${cidrsubnet(aws_vpc.main.cidr_block, 8, 30 + count.index)}"
+  availability_zone       = "${data.aws_availability_zones.available.names[count.index]}"
+  map_public_ip_on_launch = true
+  
+
+  tags = {
+    Name = "public_network"
+  }
+}
+
+#############################################################################################################
+# Attach internet gateway to created VPC
+#############################################################################################################
+resource "aws_internet_gateway" "gw" {
+  vpc_id = "${aws_vpc.main.id}"
+}
+
+#############################################################################################################
+# Set main route table for VPC to Internet Gateway
+#############################################################################################################
+resource "aws_route" "internet_access" {
+  route_table_id         = "${aws_vpc.main.main_route_table_id}"
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = "${aws_internet_gateway.gw.id}"
+}
---
+++ modules/vpc/variables.tf
@@ -0,0 +1,7 @@
+variable "custom_cidr_block" {
+  default = "172.17.0.0/16"
+}
+
+variable "subnet_count" {
+  default = 1
+}
