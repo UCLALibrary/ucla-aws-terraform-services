56ff8ba81dcfe57be2afe0e80593f43ebb510de0
Anthony <vuonga91@gmail.com>
2019-07-19 14:54:30 -0700

    not needed

--- modules/targetgroups/outputs.tf
+++
@@ -1,8 +0,0 @@
-output "cantaloupe_target_group_arn" {
-  value = "${aws_lb_target_group.cantaloupe_tg.id}"
-}
-
-output "manifeststore_target_group_arn" {
-  value = "${aws_lb_target_group.manifeststore_tg.id}"
-}
-
