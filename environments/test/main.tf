46cba9fbad53997940874dbe03d1378f520211ed
Anthony <vuonga91@gmail.com>
2019-09-06 13:04:24 -0700

    Update CF to cache all images

--- environments/test/main.tf
+++ environments/test/main.tf
@@ -140,7 +140,8 @@ module "iiif_cloudfront" {
   app_public_dns_names    = "${var.iiif_public_dns_names}"
   app_origin_id           = "ALBOrigin-${var.iiif_alb_dns_name}"
   app_ssl_certificate_arn = "${var.iiif_cloudfront_ssl_certificate_arn}"
-  app_path_pattern        = "${var.iiif_thumbnail_path_pattern}"
+  app_path_pattern        = "${var.iiif_jpg_path_pattern}"
   app_price_class         = "${var.iiif_cloudfront_price_class}"
+  default_ttl             = "${var.iiif_jpg_default_ttl}"
 }
 
