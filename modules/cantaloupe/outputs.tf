2c440d49529ba5be985753b66eb91ad3e4a05359
Anthony <vuonga91@gmail.com>
2019-10-01 12:55:16 -0700

    modules replaced by dedicated github repos

--- modules/cantaloupe/outputs.tf
+++
@@ -1,8 +0,0 @@
-output "http_listener_arn" {
-  value = "${aws_lb_listener.cantaloupe_listener.arn}"
-}
-
-output "https_listener_arn" {
-  value = "${aws_lb_listener.cantaloupe_listener_https.arn}"
-}
-
--- modules/fargate_iam_policies/variables.tf
+++
@@ -1,7 +0,0 @@
-variable "dockerauth_arn" {
-  default = "arn:aws:iam::0123456789:policy/dockerhubauth"
-}
-
-variable "app_name" {
-  default = "defaultname"
-}
