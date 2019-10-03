76087aaffe5beee3959594084bf8f42e72a59fcc
Anthony Vuong <avuong@cachemeoutside.io>
2019-10-03 11:26:25 -0700

    Have module point to TF module registry

--- environments/codebuild/main.tf
+++ environments/codebuild/main.tf
@@ -43,8 +43,9 @@ resource "aws_codebuild_project" "docker-cantaloupe" {
   }
 }
 
-module "ssm_parameters" {
-  source   = "git::https://github.com/UCLALibrary/terraform-ssm-parameters.git?ref=1.0.0"
+module "ssmparameters" {
+  source  = "app.terraform.io/UCLALibrary/ssmparameters/aws"
+  version = "1.0.0"
   ssm_list = "${var.ssm_parameters_map}"
 }
 
