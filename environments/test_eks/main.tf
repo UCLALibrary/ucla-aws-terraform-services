7ae73e18ab984d8816b5816f07e1ab2f0bd756f9
Anthony <vuonga91@gmail.com>
2020-01-27 12:50:49 -0800

    update before switching

--- environments/test_eks/main.tf
+++ environments/test_eks/main.tf
@@ -1,57 +1,99 @@
 terraform {
   required_version = "~> 0.12.0"
-  backend "remote" {}
+#  backend "remote" {}
+  backend "local" {
+    path           = "terraform.tfstate"
+  }
+}
+
+data "aws_availability_zones" "available" {
+  state = "available"
 }
 
 provider "aws" {
-  region           = "us-west-2"
+  profile          = "${var.aws_profile}"
+  region           = "${var.aws_region}"
 }
 
 resource "aws_vpc" "eks_network" {
-  cidr_block       = "172.10.0.0/16"
+  cidr_block       = "${var.eks_vpc_network}"
 
   tags             = {
     Name           = "EKS"
   }
 }
 
+resource "aws_internet_gateway" "eks_igw" {
+  vpc_id = "${aws_vpc.eks_network.id}"
+
+  tags = {
+    Name = "EKS"
+  }
+}
+
+#Example: 172.10.100.0/24
 resource "aws_subnet" "eks_subnets" {
-  vpc_id           = "${aws_vpc.eks_network}"
-  cidr_block       = "172.10.10.0/24"
+  count = length(var.eks_subnets)
+
+  vpc_id           = "${aws_vpc.eks_network.id}"
+  availability_zone = "${local.hosted_az[count.index % length(local.hosted_az)]}"
+  cidr_block       = "${var.eks_subnets[count.index]}"
 
   tags             = {
     Name           = "EKS"
   }
 }
+
+resource "aws_route_table" "eks_global_route" {
+  vpc_id = "${aws_vpc.eks_network.id}"
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.eks_igw.id}"
+  }
+
+  tags = {
+    Name = "EKS"
+  }
+}
+#  route_table_id = "${aws_route_table.eks_global_route.id}"
 
 resource "aws_key_pair" "deploy_key" {
   key_name         = "deploy-key"
   public_key       = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDTQ0YbsCQxfiUphlcardRaR3vi7fPJ4mQXFm1BfUHR+vD/s35WqbGAAXolWpVBaOXk+ix+lU5aWf0o3Op6dw7SQjYzuiRO3RMLcYBuZ6Yg1NxAthqc841XSZWCwSWc+SApROa7cVHgIQm47MUnGQGkbN0RRi7j3b0FMRif8Aga9M8VIqYw7JmUf6lZrYPGIno4r7akpYtnlMCGcKNuOu+iXFYuImzzHNpWCn6IzZVSmPYniu6KvczJv1CUNixrck7yjemZpWUDJvP7FkSsNx5ImQn4DHf5IvgQJ54htYB21mo1elCyCnlHv6SffoaXwVBM6leU5ytvGFijUDldC+4x"
 }
 
-data "aws_ami" "centos" {
+# TODO: Create security group to allow SSH from list of IPs
+
+data "aws_ami" "amazon_linux" {
   most_recent      = true
 
   filter {
-    name           = "source"
+    name           = "manifest-location"
     values         = ["amazon/amzn2-ami-hvm-2.0*"]
   }
 
   filter {
     name           = "virtualization-type"
-    values         = "hvm"
+    values         = ["hvm"]
   }
 
   # AWS Owned AMI ID
   owners           = ["137112412989"]
 }
-resource "aws_instance" "eks_bastion" {
-  ami              = "${data.aws_ami.centos}"
-  instance_type    = "t3.micro"
-  subnet_id        = "${aws_subnet.eks_subnets.id}"
-  key_name         = "${aws_key_pair.deploy_key.name}"
 
-  tags             = {
-    Name           = "EKS"
-  }
+resource "random_shuffle" "az" {
+  input = "${aws_subnet.eks_subnets[count.index].id}"
+  result_count = 2
 }
+
+#resource "aws_instance" "eks_bastion" {
+#  ami              = "${data.aws_ami.amazon_linux.id}"
+#  instance_type    = "t3.micro"
+#  subnet_id        = "${random_shuffle.az.result}"
+#  key_name         = "${aws_key_pair.deploy_key.key_name}"
+#
+#  tags             = {
+#    Name           = "EKS"
+#  }
+#}
