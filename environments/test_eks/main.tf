3fc01440d56465854bde4dc477c39851d1676cff
Anthony <avuong@tallgeese.zero>
2020-01-30 13:21:31 -0800

    Update before switch

--- environments/test_eks/main.tf
+++ environments/test_eks/main.tf
@@ -38,6 +38,7 @@ resource "aws_subnet" "eks_subnets" {
   vpc_id           = "${aws_vpc.eks_network.id}"
   availability_zone = "${local.hosted_az[count.index % length(local.hosted_az)]}"
   cidr_block       = "${var.eks_subnets[count.index]}"
+  map_public_ip_on_launch = "true"
 
   tags             = {
     Name           = "EKS"
@@ -56,14 +57,46 @@ resource "aws_route_table" "eks_global_route" {
     Name = "EKS"
   }
 }
-#  route_table_id = "${aws_route_table.eks_global_route.id}"
+
+resource "aws_route_table_association" "eks_route_global_table" {
+  for_each = toset(aws_subnet.eks_subnets.*.id)
+  subnet_id = each.key
+  route_table_id = "${aws_route_table.eks_global_route.id}"
+
+  depends_on = [
+    aws_subnet.eks_subnets
+  ]
+}
 
 resource "aws_key_pair" "deploy_key" {
   key_name         = "deploy-key"
-  public_key       = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDTQ0YbsCQxfiUphlcardRaR3vi7fPJ4mQXFm1BfUHR+vD/s35WqbGAAXolWpVBaOXk+ix+lU5aWf0o3Op6dw7SQjYzuiRO3RMLcYBuZ6Yg1NxAthqc841XSZWCwSWc+SApROa7cVHgIQm47MUnGQGkbN0RRi7j3b0FMRif8Aga9M8VIqYw7JmUf6lZrYPGIno4r7akpYtnlMCGcKNuOu+iXFYuImzzHNpWCn6IzZVSmPYniu6KvczJv1CUNixrck7yjemZpWUDJvP7FkSsNx5ImQn4DHf5IvgQJ54htYB21mo1elCyCnlHv6SffoaXwVBM6leU5ytvGFijUDldC+4x"
+  public_key       = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDkwazJbdOiBSp3Ylz6X6ltkQ7HgmEU9fimotzrBWGohO7ss73bqPjWTR4nLPVGcuyLG51i937YSijvbG9QD1S7D4dN780+SB4UfX4cRKNYIw2XCGwfsWYHOGB8Gl/PRtPx8PA5DST4qy+dztPE2zVzAt6ChIw1jxSocdbJ9gd4IF7U6jK3ziywpFMhDfLs/vlx98Fm521xYWtefaT1+bb4a7/YdE++6KA/sbk6Dg3rzlfHHmpWJMromL/wHnIj/njBW2LlTIIkvJY+gLbVYQ3QDrbxbKa5iHaBTI7P6iRkJhkssE4VEpm/y79XD11EZd8E2GsAs2WjnTfynIN13Reh"
 }
 
-# TODO: Create security group to allow SSH from list of IPs
+
+resource "aws_security_group" "allow_ssh" {
+  name = "allow_ssh"
+  description = "Allow SSH access from worldwide"
+  vpc_id = "${aws_vpc.eks_network.id}"
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "EKS"
+  }
+}
 
 data "aws_ami" "amazon_linux" {
   most_recent      = true
@@ -82,18 +115,50 @@ data "aws_ami" "amazon_linux" {
   owners           = ["137112412989"]
 }
 
-resource "random_shuffle" "az" {
-  input = "${aws_subnet.eks_subnets[count.index].id}"
-  result_count = 2
+resource "aws_instance" "eks_bastion" {
+  ami              = "${data.aws_ami.amazon_linux.id}"
+  instance_type    = "t3.micro"
+  subnet_id        = "${aws_subnet.eks_subnets[0].id}"
+  key_name         = "${aws_key_pair.deploy_key.key_name}"
+  vpc_security_group_ids = ["${aws_security_group.allow_ssh.id}"]
+
+  tags             = {
+    Name           = "EKS"
+  }
 }
 
-#resource "aws_instance" "eks_bastion" {
-#  ami              = "${data.aws_ami.amazon_linux.id}"
-#  instance_type    = "t3.micro"
-#  subnet_id        = "${random_shuffle.az.result}"
-#  key_name         = "${aws_key_pair.deploy_key.key_name}"
-#
-#  tags             = {
-#    Name           = "EKS"
-#  }
-#}
+data "aws_iam_policy_document" "eks_assume_policy_document" {
+  statement {
+    actions = ["sts:AssumeRole"]
+ 
+    principals {
+      type = "Service"
+      identifiers = ["eks.amazonaws.com"]
+    }
+  }
+}
+
+data "template_file" "iam_policy_permissions_for_lambda" {
+  template = "${file("${path.module}/templates/iam_policy.json.tpl")}"
+  vars = {
+    s3_list_of_buckets = "${jsonencode(var.s3_iam_allowed_resources)}"
+    s3_permissions = "${jsonencode(var.s3_iam_allowed_actions)}"
+    cloudwatch_permissions = "${jsonencode(var.cloudwatch_iam_allowed_actions)}"
+  }
+}
+
+resource "aws_iam_role" "iam_for_eks" {
+  name = "iam_for_eks"
+  assume_role_policy = "${data.aws_iam_policy_document.eks_assume_policy_document}"
+} 
+
+resource "aws_eks_cluster" "eks_cluster" {
+  name = "eks_cluster"
+  role_arn = "${var.aws_iam_role.iam_for_eks.role_arn}"
+  
+  vpc_config {
+    subnet_ids = [
+      toset(aws_subnets.eks_subnets.*.id)
+    ]
+  }
+}
