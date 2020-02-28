4b35b90ff783ae54abd261979d479cbcd920d9bd
Anthony Vuong <avuong@cachemeoutside.io>
2020-02-28 15:23:00 -0800

    Add ALB Ingress policy

--- environments/test/eks-iam/main.tf
+++ environments/test/eks-iam/main.tf
@@ -20,7 +20,7 @@ data "aws_iam_policy_document" "eks_assume_policy_document" {
 }
 
 resource "aws_iam_role" "iam_for_eks" {
-  name = "terraform_eks_test-eks"
+  name = "test-eks"
   assume_role_policy = data.aws_iam_policy_document.eks_assume_policy_document.json
 } 
 
@@ -35,7 +35,7 @@ resource "aws_iam_role_policy_attachment" "eks_attach_cluster_policy" {
 }
 
 resource "aws_iam_role" "iam_for_eks_node_group" {
-  name = "terraform_nodegroup_test-eks"
+  name = "nodegroup_test-eks"
 
   assume_role_policy = jsonencode({
     Statement = [{
@@ -62,5 +62,15 @@ resource "aws_iam_role_policy_attachment" "iam_for_eks_node_group-AmazonEKS_CNI_
 resource "aws_iam_role_policy_attachment" "iam_for_eks_node_group-AmazonEC2ContainerRegistryReadOnly" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
   role       = aws_iam_role.iam_for_eks_node_group.name
+}
+
+resource "aws_iam_policy" "alb_ingress_policy" {
+  name = "Test-EKS-ALBIngressController"
+  policy = file("policies/ALBIngressController.json")
+}
+
+resource "aws_iam_role_policy_attachment" "iam_for_alb_ingress" {
+  policy_arn = aws_iam_policy.alb_ingress_policy.arn
+  role = aws_iam_role.iam_for_eks.name
 }
 
