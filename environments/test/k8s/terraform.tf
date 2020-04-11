1d8e334c0aa66f38b63f7d1f4b35b9b89acd5444
Anthony Vuong <avuong@cachemeoutside.io>
2020-04-10 19:44:09 -0700

    Update backend to use s3

--- environments/test/k8s/terraform.tf
+++ environments/test/k8s/terraform.tf
@@ -1,11 +1,11 @@
 terraform {
   required_version = "~> 0.12.20"
-  backend "remote" {}
-#  backend "s3" {
-#    encrypt = true
-#    bucket = "prod-services-terraform-state"
-#    region = "us-west-2"
-#    key = "test/iiif-deployment"
-#    dynamodb_table = "terraform-state-lock-dynamo"
-#  }
+#  backend "remote" {}
+  backend "s3" {
+    encrypt = true
+    bucket = "prod-services-terraform-state"
+    region = "us-west-2"
+    key = "test/eks-cantaloupe-deployment"
+    dynamodb_table = "prod-services-terraform-state-lock"
+  }
 }
