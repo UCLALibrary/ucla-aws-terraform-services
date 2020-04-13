cebcb26b36b632d2e72cded6cc9c713b4396ead0
Anthony Vuong <avuong@cachemeoutside.io>
2020-04-12 23:59:52 -0700

    Update policies

--- environments/iam/codebuild.tf
+++ environments/iam/codebuild.tf
@@ -34,14 +34,26 @@ resource "aws_iam_role_policy_attachment" "eks_attach_codebuild_default_policy" 
   policy_arn = aws_iam_policy.codebuild_default_policy.arn
 }
 
-# Create IAM policy to allow CodeBuild to access EKS cluster information
+# Create IAM policy to allow EKS CodeBuild role to access EKS cluster information
 resource "aws_iam_policy" "codebuild_allow_eks_cluster_get" {
   name = "${var.prefix_tag}_codebuild-get-eks-cluster-info"
   policy = file("policies/codebuild-allow-eks-get.json")
 }
 
-# Attach EKS list cluster information permission to shared CodeBuild role
+# Attach EKS list cluster information permission to shared EKS CodeBuild role
 resource "aws_iam_role_policy_attachment" "attach_eks_cluster_get_policy" {
   role = aws_iam_role.eks_shared_codebuild_role.name
   policy_arn = aws_iam_policy.codebuild_allow_eks_cluster_get.arn
+}
+
+# Create IAM Policy for EKS CodeBuild role to access Terraform S3 backend
+resource "aws_iam_policy" "eks_access_s3_backend" {
+  name = "${var.prefix_tag}_codebuild-access-s3-backend"
+  policy = templatefile("policies/codebuild-access-terraform-state-bucket.json.template", { TERRAFORM_STATE_BUCKET = var.terraform_state_bucket, TERRAFORM_LOCK_TABLE_DYNAMODB = var.terraform_state_lock_table })
+}
+
+# Attach Terraform S3 backend state permissions to EKS CodeBuild role
+resource "aws_iam_role_policy_attachment" "attach_eks_access_s3_backend" {
+  role = aws_iam_role.eks_shared_codebuild_role.name
+  policy_arn = aws_iam_policy.eks_access_s3_backend.arn
 }
--- environments/iam/variables.tf
+++ environments/iam/variables.tf
@@ -1,5 +1,16 @@
 variable "aws_region" {
+  type = string
   default = "us-west-2"
+}
+
+variable "terraform_state_lock_table" {
+  type = string
+  default = "dynamodb_state_lock_table"
+}
+
+variable "terraform_state_bucket" {
+  type = string
+  default = "s3_state_bucket"
 }
 
 variable "eks_deployment_robot_name" {
